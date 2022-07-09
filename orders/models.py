3e28fdf9a456128ab86c98cd2e3e657a4edde73e
rebel216 <neerajbhardwajpkt@gmail.com>
2022-07-09 22:16:21 +0530

    get api

--- orders/models.py
+++ orders/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Data(models.Model):
     user_slug = models.CharField(max_length=200)
-    filename = models.FileField(upload_to='uploads')
+    filename = models.CharField(max_length=200)
