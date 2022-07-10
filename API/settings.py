08da2b132d83775bfcf0e03f92b3387b26ff8a7b
rebel216 <neerajbhardwajpkt@gmail.com>
2022-07-10 16:22:52 +0530

    post req

--- API/settings.py
+++ API/settings.py
@@ -92,7 +92,12 @@ WSGI_APPLICATION = 'API.wsgi.application'
 
 
 
-DATABASES = {}
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR,'db.sqlite3')
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
