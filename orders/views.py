9fec86ad18d8d2836c4b14543cabf4498ebb33e1
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-08 21:40:39 +0530

    Postgres support

--- orders/views.py
+++ orders/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from .form import UploadFileForm
 from django.conf import settings
 import requests 
+from .form import UploadFileForm
 
 url = "https://app.bluevine.com/api/v3/partners/file-upload/?external_register_token=PLATFORMFUNDINGTEST-2a1e0af3691a4667a2b6658db3dfce62&pid_login=0182857"
 
@@ -29,29 +30,32 @@ class DataView(APIView):
         print(request.FILES)       
         if serializer.is_valid():
             serializer.save()
-            # request.headers['Content_Length'] = len(request.headers)
-            print(len(request.headers))
-            User_slug = request.headers.get('slug')
-            file_up = request.FILES.get('file')
-            
-            print(User_slug)
-            fs = FileSystemStorage()
-            filename = fs.save("sample.pdf", file_up)
-            uploaded_file_url = fs.url(filename)
+            if request.method == 'POST':
+                form = UploadFileForm(request.POST, request.FILES)
+                # request.headers['Content_Length'] = len(request.headers)
+                print(len(request.headers))
+                User_slug = request.headers.get('slug')
+                file_up = request.FILES.get('file')
+                
+                print(User_slug)
+                fs = FileSystemStorage()
+                filename = fs.save("sample.pdf", file_up)
+                uploaded_file_url = fs.url(filename)
 
-            files = [
-                ('file', ('Partner Web Service documentation v15 08112021docx (2).pdf',
-                          open(settings.MEDIA_ROOT+uploaded_file_url, 'rb'), 'application/pdf'))
-            ]
-            print(uploaded_file_url)
-            payload = {'category': 'bank_statement',
-                       'user_slug': User_slug,
-                       'original_filename': 'bank_statement.pdf',
-                       'upload_for': 'user_registration'}
-            response = requests.request(
-            "POST", url, headers=headers, data=payload,files=files)
-            return Response(data={response}, status=status.HTTP_200_OK)
-            
+                files = [
+                    ('file', ('Partner Web Service documentation v15 08112021docx (2).pdf',
+                              open(settings.MEDIA_ROOT+uploaded_file_url, 'rb'), 'application/pdf'))
+                ]
+                print(uploaded_file_url)
+                payload = {'category': 'bank_statement',
+                           'user_slug': User_slug,
+                           'original_filename': 'bank_statement.pdf',
+                           'upload_for': 'user_registration'}
+                response = requests.request(
+                "POST", url, headers=headers, data=payload,files=files)
+                return Response(data={response}, status=status.HTTP_200_OK)
+            return Response({"status": "xxx", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
         else:
             print(serializer.errors)
             return Response({"status": "xxx", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
