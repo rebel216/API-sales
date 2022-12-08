0f07be640a5e13cfeb4086d719392f995f443822
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-08 19:13:53 +0530

    Postgres support

--- API/settings.py
+++ API/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 import dj_database_url
 import os
 
-conn_max_age
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,7 +103,7 @@ DATABASES = {
        'PORT': '5431',
    }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
